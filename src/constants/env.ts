5ddb94e5a939a95e51927c61481092de5495aeb4
carolusian <pkufashuo400@gmail.com>
2019-03-10 00:10:28 +0800

    Add develop env

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,14 +1,14 @@
 export const __DEV__ =
   !process.env.REACT_APP_ENV || process.env.REACT_APP_ENV === 'dev'
 export const __TEST__ = process.env.REACT_APP_ENV === 'test'
-export const __STAGE__ = process.env.REACT_APP_ENV === 'stage'
+export const __DEVELOP__ process.env.REACT_APP_ENV === 'develop'
 export const __PROD__ = process.env.REACT_APP_ENV === 'production'
 
 export const API_ENDPOINT = __DEV__
   ? 'http://localhost:4000'
   : __TEST__
   ? 'http://localhost:4000'
-  : __STAGE__
-  ? 'https://server-stage.matters.news'
+  : __DEVELOP__
+  ? 'https://server-develop.matters.news'
   : __PROD__
   ? 'https://server.matters.news'
