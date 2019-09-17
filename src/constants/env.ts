68e5659d868a4d29f4c1278e76c25cfe71a4e057
RoberTu <robertu0717@gmail.com>
2019-09-17 12:16:05 +0800

    [hotfix] env config

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,15 +1,15 @@
-export const __DEV__ =
-  !process.env.REACT_APP_ENV || process.env.REACT_APP_ENV === 'dev'
+export const __LOCAL__ =
+  !process.env.REACT_APP_ENV || process.env.REACT_APP_ENV === 'local'
 export const __TEST__ = process.env.REACT_APP_ENV === 'test'
 export const __DEVELOP__ = process.env.REACT_APP_ENV === 'develop'
 export const __PROD__ = process.env.REACT_APP_ENV === 'production'
 
-export const API_ENDPOINT = __DEV__
+export const API_ENDPOINT = __LOCAL__
   ? 'http://localhost:4000/graphql'
   : __TEST__
   ? 'http://localhost:4000/graphql'
   : __DEVELOP__
-  ? 'http://localhost:4000/graphql'
+  ? 'https://server-stage.matters.news/graphql'
   : __PROD__
   ? 'https://server.matters.news/graphql'
   : ''
