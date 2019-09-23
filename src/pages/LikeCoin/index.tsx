c09077812b06da2da7accefa0779743560eef998
RoberTu <robertu0717@gmail.com>
2019-09-23 23:52:37 +0800

    Add generate likerIds trigger

---
+++ src/pages/LikeCoin/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+import GenerateTempLikerIds from './GenerateTempLikerIds'
+
+const LikeCoin = () => {
+  return (
+    <>
+      <GenerateTempLikerIds />
+    </>
+  )
+}
+export default LikeCoin
