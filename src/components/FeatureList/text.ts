1d14adea8f5d902198d084cbde5bab322e4b84ac
Zeck Li <devformatters@gmail.com>
2020-06-15 15:18:27 +0800

    Add feature flag for fingerprint.

--- src/components/FeatureList/text.ts
+++ src/components/FeatureList/text.ts
@@ -2,7 +2,8 @@ export const nameMap = {
   verify_appreciate: '讚賞保護',
   payout: '提現',
   add_credit: '充值',
-  payment: '支付'
+  payment: '支付',
+  fingerprint: '指紋',
 }
 
 export const explanationMap = {
@@ -10,5 +11,6 @@ export const explanationMap = {
     '通過ReCaptcha限制讚賞只能在網站上手動進行，不能通過腳本自動觸發。',
   payout: '用戶可以在滿足條件時從法幣錢包裡提現。',
   add_credit: '用戶可以向法幣錢包中充值。',
-  payment: '用戶可以向另一個用戶支付法幣，目前僅有文章的「支持作者」按鈕。'
+  payment: '用戶可以向另一個用戶支付法幣，目前僅有文章的「支持作者」按鈕。',
+  fingerprint: '透過已紀錄指紋，不發送驗證碼給被封禁用戶。',
 }
