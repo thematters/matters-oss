cff2ed5e29c5d13f7f5af57f274b97b0ef0fbc83
Zeck Li <devformatters@gmail.com>
2021-01-27 22:24:56 +0800

    feat(circle): revise feature flag for Circle

--- src/components/FeatureList/index.tsx
+++ src/components/FeatureList/index.tsx
@@ -19,7 +19,8 @@ const nameMap = {
   payment: '支付',
   fingerprint: '指紋',
   tag_adoption: '標籤認領',
-  circle: '圍爐',
+  circle_management: '圍爐管理',
+  circle_interact: '圍爐交互',
 }
 
 const explanationMap = {
@@ -30,7 +31,8 @@ const explanationMap = {
   payment: '用戶可以向另一個用戶支付法幣，目前僅有文章的「支持作者」按鈕。',
   fingerprint: '透過已紀錄指紋，不發送驗證碼給被封禁用戶。',
   tag_adoption: '用戶可以認領無人主理的標籤',
-  circle: '用戶可以使用圍爐功能',
+  circle_management: '用戶可以使用圍爐搭建、管理功能',
+  circle_interact: '用戶可以於圍爐追蹤、訂閱、發布眾聊等',
 }
 
 class FeatureList extends React.Component<FeatureListProps> {
