a4a326a593ef0cf2c6cac49e285768f62cf072a6
xdj <xudongjie@laixi.pro>
2023-04-10 11:23:08 +0800

    feat(user-restriction): init restricted-user list page

---
+++ src/pages/RestrictedUserList/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export default class RestrictedUserList extends React.Component {
+  public render() {
+    return <div>helloworld</div>
+  }
+}
--- src/routes/user.tsx
+++ src/routes/user.tsx
@@ -6,6 +6,7 @@ import Private from '../components/Private'
 import UserList from '../pages/UserList'
 import UserDetail from '../pages/UserDetail'
 import BadgedUserList from '../pages/BadgedUserList'
+import RestrictedUserList from '../pages/RestrictedUserList'
 
 import { PATH } from '../constants'
 
@@ -18,6 +19,18 @@ const userRoutes = [
         <Layout.Header />
         <Layout.Content>
           <UserList {...props} />
+        </Layout.Content>
+      </Private>
+    ),
+  },
+  {
+    path: PATH.RESTRICTED_USER_LIST,
+    exact: true,
+    component: (props: RouteConfigComponentProps) => (
+      <Private>
+        <Layout.Header />
+        <Layout.Content>
+          <RestrictedUserList {...props} />
         </Layout.Content>
       </Private>
     ),
