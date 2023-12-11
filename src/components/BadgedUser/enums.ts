7c3bcdcd9cd373c4b48a05994b3da85292c2aaf0
49659410+tx0c <>
2023-12-11 14:08:30 +0000

    feat(nomad-badges): oss settings nomad badges:
    Implementation of Digital Nomad program #4049
    
    for thematters/developer-resource#326

--- src/components/BadgedUser/enums.ts
+++ src/components/BadgedUser/enums.ts
@@ -6,4 +6,8 @@ export const USER_BADGES: {
   seed: { text: '種子用戶', color: '#F3D26E' },
   golden_motor: { text: '金馬達', color: '#C0A46B' },
   architect: { text: '馬特市建築師', color: '#DC752B' },
+  nomad1: { text: '月之夢', color: '#00447c' },
+  nomad2: { text: '流星號', color: '#066320' },
+  nomad3: { text: '光輪號', color: '#5b0707' },
+  nomad4: { text: '火閃電', color: '#bf9604' },
 }
